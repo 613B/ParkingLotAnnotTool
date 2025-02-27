0b8bf7ebb82c65858d52625f20d3b200b39cc2c6
613B <fujita4re@gmail.com>
2025-02-27 14:03:17 +0900

    [enhancement] hotkey.py, using pynput instead of keyboard

--- ParkingLotAnnotTool/public/hotkey.py
+++ ParkingLotAnnotTool/public/hotkey.py
@@ -1,37 +1,36 @@
-import keyboard
+from pynput import keyboard
 from PyQt6.QtCore import QObject, pyqtSignal, Qt
 
 
 class HotKey(QObject):
-
     hotkey_signal = pyqtSignal(int)
 
     def __init__(self):
         super(HotKey, self).__init__()
-        keyboard.add_hotkey("h", self.key_h)
-        keyboard.add_hotkey("j", self.key_j)
-        keyboard.add_hotkey("k", self.key_k)
-        keyboard.add_hotkey("l", self.key_l)
-        keyboard.add_hotkey("left", self.key_left)
-        keyboard.add_hotkey("right", self.key_right)
+        self.listener = keyboard.Listener(on_press=self.on_press)
+        self.listener.start()
 
-    def key_h(self):
-        self.hotkey_signal.emit(Qt.Key.Key_H)
+    def on_press(self, key):
+        key_mapping = {
+            'h': Qt.Key.Key_H,
+            'j': Qt.Key.Key_J,
+            'k': Qt.Key.Key_K,
+            'l': Qt.Key.Key_L,
+            keyboard.Key.left: Qt.Key.Key_Left,
+            keyboard.Key.right: Qt.Key.Key_Right
+        }
 
-    def key_j(self):
-        self.hotkey_signal.emit(Qt.Key.Key_J)
+        try:
+            if isinstance(key, keyboard.KeyCode):  # 文字キーの場合
+                key_char = key.char
+            else:  # 矢印キーなどの特殊キー
+                key_char = key
 
-    def key_k(self):
-        self.hotkey_signal.emit(Qt.Key.Key_K)
+            if key_char in key_mapping:
+                self.hotkey_signal.emit(key_mapping[key_char])
 
-    def key_l(self):
-        self.hotkey_signal.emit(Qt.Key.Key_L)
-
-    def key_left(self):
-        self.hotkey_signal.emit(Qt.Key.Key_Left)
-
-    def key_right(self):
-        self.hotkey_signal.emit(Qt.Key.Key_Right)
+        except AttributeError:
+            pass  # 予期しないキーは無視
 
 
 def initialize():
